d4ec25813f27454405c025945eed44b2271b3b13
Tonyvitamin <a30942@gmail.com>
2021-07-23 10:35:57 +0800

    modified:   backtest_result/2887.TW.html 	new file:   data/stock_id.csv 	modified:   requirements.txt 	new file:   tw_list.py 	modified:   tw_stock.py

--- tw_stock.py
+++ tw_stock.py
@@ -2,8 +2,15 @@ import yfinance as yf
 import  pandas as pd
 from pandas_datareader import data
 import datetime
+import csv 
 
 yf.pdr_override()
+
+stock_list = []
+with open("data/stock_id.csv") as f:
+    for row in f:
+        stock_list.append(row.split()[0])
+
 
 target_stock = '2887.TW'
 
@@ -13,3 +20,6 @@ df = data.get_data_yahoo([target_stock], start_date, end_date)
 
 filename = f'./data/{target_stock}.csv'
 df.to_csv(filename)
+
+
+
